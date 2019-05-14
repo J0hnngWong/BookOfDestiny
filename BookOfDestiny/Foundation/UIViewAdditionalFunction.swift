f3f6337fa62ac4281c858b1e238f3d3f63e84922
王嘉宁 <wangjianing@imdada.cn>
2019-05-14 19:03:23 +0800

    新建UIView扩展， 加入渐变颜色代码

---
+++ BookOfDestiny/Foundation/UIViewAdditionalFunction.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewAdditionalFunction.swift
+//  BookOfDestiny
+//
+//  Created by 王嘉宁 on 2019/5/14.
+//  Copyright © 2019 王嘉宁. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    public func addGradientColor(with colorArray: Array<UIColor>, circulation: Bool) {
+        for color: UIColor in colorArray {
+            
+        }
+    }
+}
